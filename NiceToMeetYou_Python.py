0400d7102eeb5da990a19d4ec4f8998d09c9c367
leonguevara <leon.guevara@outlook.com>
2017-08-18 12:54:47 -0500

    Initial commit

---
+++ NiceToMeetYou_Python.py
@@ -0,0 +1,15 @@
+# NiceToMeetYou_Python.py
+#
+# This program will ask the user for their name and it will greet them
+# using their name.
+#
+# Python interpreter: 3.6
+#
+# Author: León Felipe Guevara Chávez
+# email:  leon.guevara@itesm.mx
+# date:   May 20, 2017
+#
+
+userName = input("What is your name?")
+message = "Hello " + userName + "! Nice to meet you."
+print(message)
